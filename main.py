0775c16c29148696733ccf3b2d47699f64f170ed
Hanfeng Zhu <zhuhanfeng@gmail.com>
2017-12-20 17:47:49 +0800

    make it more compatible with google tensorflow

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.contrib.learn import learn_runner
 
-def __process_data(training_file, eval_file):
+
+def _process_data(training_file, eval_file):
   training_set = pd.read_csv(training_file, header=0)
   eval_set = pd.read_csv(eval_file, header=0)
   feature_names = [f for f in list(training_set) if 'feature' in f]
   return training_set, eval_set, feature_names
 
-def __build_train_input_fn(data_set, feature_names):
+
+def _build_train_input_fn(data_set, feature_names):
   return tf.estimator.inputs.pandas_input_fn(
       x=data_set[feature_names],
       y=data_set['target'],
@@ -19,7 +21,7 @@ def __build_train_input_fn(data_set, feature_names):
       shuffle=True)
 
 
-def __build_eval_input_fn(data_set, feature_names):
+def _build_eval_input_fn(data_set, feature_names):
   return tf.estimator.inputs.pandas_input_fn(
       x=data_set[feature_names],
       y=data_set['target'],
@@ -27,44 +29,38 @@ def __build_eval_input_fn(data_set, feature_names):
       shuffle=True)
 
 
-def __experiment_fn(run_config, hparams):
-  training_set, eval_set, feature_names = __process_data(
-      training_file='data/numerai_training_data.csv',
-      eval_file='data/numerai_tournament_data.csv')
+def _experiment_fn(run_config, hparams):
+  training_set, eval_set, feature_names = _process_data(
+      training_file=('/usr/local/google/home/hanfeng/Desktop/tensorflow/'
+                     'numerai/data/numerai_training_data.csv'),
+      eval_file=('/usr/local/google/home/hanfeng/Desktop/tensorflow/'
+                 'numerai/data/numerai_tournament_data.csv'))
 
   validation_set = eval_set.loc[eval_set.data_type == 'validation']
-  test_set = eval_set.loc[eval_set.data_type == 'test']
-  live_set = eval_set.loc[eval_set.data_type == 'live']
-
-  #feature_columns = [tf.feature_column.numeric_column(k)
-  #                   for k in feature_names]
+  # test_set = eval_set.loc[eval_set.data_type == 'test']
+  # live_set = eval_set.loc[eval_set.data_type == 'live']
 
   return tf.contrib.learn.Experiment(
       estimator=numer_estimator.NumerEstimator(run_config, hparams),
-    #  estimator=tf.estimator.DNNClassifier(
-    #      feature_columns=feature_columns,
-    #      hidden_units=[1024, 512, 256],
-    #      config=run_config,
-    #  ),
-      train_input_fn=__build_train_input_fn(training_set, feature_names),
-      eval_input_fn=__build_eval_input_fn(validation_set, feature_names),
+      train_input_fn=_build_train_input_fn(training_set, feature_names),
+      eval_input_fn=_build_eval_input_fn(validation_set, feature_names),
       train_steps=10001)
 
 
-def main():
+def main(_):
   config = tf.contrib.learn.RunConfig(
-      model_dir='/usr/local/google/home/hanfeng/Desktop/tensorflow/numerai/logs',
+      model_dir='/tmp/numerai/logs',
       save_summary_steps=100,
       save_checkpoints_steps=100)
 
-  params = tf.contrib.training.HParams(
-      learning_rate=0.0005)
+  params = tf.contrib.training.HParams(learning_rate=0.00005)
 
-  learn_runner.run(experiment_fn=__experiment_fn,
-                   run_config=config,
-                   hparams=params,
-                   schedule='train_and_evaluate')
+  learn_runner.run(
+      experiment_fn=_experiment_fn,
+      run_config=config,
+      hparams=params,
+      schedule='train_and_evaluate')
 
 
 if __name__ == '__main__':
-  main()
+  tf.app.run()
--- numer_estimator.py
+++ numer_estimator.py
@@ -1,24 +1,22 @@
 """A general tensorflow estimator for numerai models."""
 
-import copy
 import functools
 import tensorflow as tf
-from tensorflow.contrib.learn import ModeKeys
-from tensorflow.python.estimator import estimator
-from tensorflow.python.feature_column import feature_column
+
 
 def _build_network(features):
   # Returns logits with shape [None, 1]
-  feature_columns = [tf.feature_column.numeric_column(k)
-                     for k in list(features)]
-  input_layer = feature_column.input_layer(features, feature_columns)
-  fc1 = tf.layers.dense(inputs=input_layer, units=1024,
-                        activation=tf.nn.relu, name='fc1')
-  fc2 = tf.layers.dense(inputs=fc1, units=512,
-                        activation=tf.nn.relu, name='fc2')
+  feature_columns = [
+      tf.feature_column.numeric_column(k) for k in list(features)
+  ]
+  input_layer = tf.feature_column.input_layer(features, feature_columns)
+  fc1 = tf.layers.dense(
+      inputs=input_layer, units=1024, activation=tf.nn.relu, name='fc1')
+  fc2 = tf.layers.dense(
+      inputs=fc1, units=512, activation=tf.nn.relu, name='fc2')
   logits = tf.layers.dense(inputs=fc2, units=1, name='logits')
-  #return tf.squeeze(logits, [1])
   return logits
+
 
 def _build_train_op(loss, params):
   lr_decay_fn = functools.partial(
@@ -37,10 +35,9 @@ def _build_train_op(loss, params):
 
 def _build_eval_metric_ops(labels, predictions):
   return {
-      'Accuracy': tf.metrics.accuracy(
-          labels=labels,
-          predictions=predictions,
-          name='accuracy')
+      'Accuracy':
+          tf.metrics.accuracy(
+              labels=labels, predictions=predictions, name='accuracy')
   }
 
 
@@ -49,8 +46,9 @@ def _maybe_expand_dims(tensor):
     tensor = tf.expand_dims(tensor, -1)
   return tensor
 
+
 def _numer_model_fn(features, labels, mode, params):
-  is_training = mode == ModeKeys.TRAIN
+  is_training = mode == tf.estimator.ModeKeys.TRAIN
   labels = _maybe_expand_dims(labels)
   logits = _build_network(features)
   predictions = tf.cast(tf.nn.sigmoid(logits) > 0.5, tf.int64)
@@ -64,15 +62,13 @@ def _numer_model_fn(features, labels, mode, params):
       eval_metric_ops=_build_eval_metric_ops(labels, predictions))
 
 
-class NumerEstimator(estimator.Estimator):
+class NumerEstimator(tf.estimator.Estimator):
 
   def __init__(self, config=None, params=None):
+
     def _model_fn(features, labels, mode, params):
       return _numer_model_fn(
-          features=features,
-          labels=labels,
-          mode=mode,
-          params=params)
+          features=features, labels=labels, mode=mode, params=params)
+
     super(NumerEstimator, self).__init__(
         model_fn=_model_fn, config=config, params=params)
-
